29fc209b935f2a6d7e8c15314b01d340ea6a3c88
Kieran Emery <kizwiz@hotmail.co.uk>
2024-05-08 21:48:17 +0100

    More DI examples.

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -4,19 +4,25 @@
     {
         static void Main(string[] args)
         {
-            /* New instance of the FileLogger class and assigning it to a variable called logger.
-             *
-            */
+            // Demonstrate Dependency Inversion by injecting different loggers into CustomerService.
+
             ILogger logger = new FileLogger();
-
-            /* Creates a new instance of the CustomerService class and passes the logger instance as a parameter to its constructor.
-             * The CustomerService class implements the ICustomerService interface and requires an instance of an ILogger implementation to be provided during instantaiton.
-             * */
             ICustomerService customerService = new CustomerService(logger);
-
-            /* Calls the Add method on the customerService object. Since customerService is an instance of the CustomerService class, the Add method defined in the CustomerService class is invoked.
-             */
             customerService.Add();
+
+            // Use FileLogger
+            ILogger fileLogger = new FileLogger();
+            ICustomerService fileCustomerService = new CustomerService(fileLogger);
+            fileCustomerService.Add();
+
+            // Use EmailLogger
+            ILogger emailLogger = new EmailLogger();
+            ICustomerService emailCustomerService = new CustomerService(emailLogger);
+            emailCustomerService.Add();
+            // Use EventViewerLogger
+            ILogger eventViewerLogger = new EventViewerLogger();
+            ICustomerService eventViewerCustomerService = new CustomerService(eventViewerLogger);
+            eventViewerCustomerService.Add();
         }
     }
 }
